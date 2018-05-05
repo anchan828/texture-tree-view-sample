36c1f0174b91b4da0c8c75d3fdffcab05311057b
kyusyukeigo <anchan828@gmail.com>
2018-05-06 07:22:29 +0900

    HierarchyViewサンプル追加

---
+++ Assets/Editor/HierarchyView/HierarchyView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+public class HierarchyView : TreeView
+{
+    public HierarchyView(TreeViewState state) : base(state)
+    {
+    }
+
+    protected override TreeViewItem BuildRoot()
+    {
+        // Root の depth は -1
+        var root = new TreeViewItem { id = 0, depth = -1 };
+
+		// AddChild メソッド、または children プロパティで開閉で表示可能な子要素を追加
+        root.AddChild(
+            new TreeViewItem
+            {
+                id = 1,
+                depth = 0,
+                displayName = "Animals",
+                children = new List<TreeViewItem> {
+                    new TreeViewItem {id = 2, depth = 1, displayName = "Mammals"},
+                }
+            }
+        );
+        // 以下でも同様の実装が行うことが出来る
+        // var rows = new List<TreeViewItem> {
+        //     new TreeViewItem {id = 1, depth = 0, displayName = "Animals"},
+        //     new TreeViewItem {id = 2, depth = 1, displayName = "Mammals"},
+        // };
+        // Listに追加した順番とdepthによって親子関係を構築できる便利メソッド
+        // SetupParentsAndChildrenFromDepths(root, rows);
+
+        return root;
+    }
+}
---
+++ Assets/Editor/HierarchyView/HierarchyWindow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+public class HierarchyWindow : EditorWindow
+{
+
+    [MenuItem("TreeViewSample/HierarchyWindow")]
+    static void Open()
+    {
+        GetWindow<HierarchyWindow>();
+    }
+    private HierarchyView hierarchyView;
+
+    void OnEnable()
+    {
+		// 開閉状態や、どのTreeViewItemを選択しているかなどの状態が格納されるstateオブジェクト
+        var state = new TreeViewState();
+		
+        hierarchyView = new HierarchyView(state);
+
+		// 初期化のために呼び出す
+        hierarchyView.Reload();
+    }
+
+    void OnGUI()
+    {
+        hierarchyView.OnGUI(new Rect(Vector2.zero, position.size));
+    }
+}
